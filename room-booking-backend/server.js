2432c84fc353377447626496a009710e06ffe6bb
Aryan@2701 <aryanag2701@gmail.com>
2024-06-17 16:37:42 +0530

    adding

--- room-booking-backend/server.js
+++ room-booking-backend/server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const cors = require('cors')
 const { v4: uuidv4 } = require('uuid')
 
 const app = express()
 app.use(bodyParser.json())
-app.use(cors())
 
 let bookings = []
 
